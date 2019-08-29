ab78add532395a7a9ebd199ea4c5b936f0514392
Liza <elis.liza567@gmail.com>
2019-08-29 11:29:20 +0300

    First

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'components/header.php'; ?>
+<main>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis tenetur placeat sapiente consequuntur explicabo. Sunt incidunt, quis quisquam suscipit nobis fuga quae excepturi animi dolores dignissimos reprehenderit enim perspiciatis qui deleniti nihil, reiciendis dolorem ullam modi quod quo. Cumque, mollitia recusandae, vitae dicta eveniet minus sed obcaecati laboriosam ducimus atque voluptatem ipsa quidem repudiandae, facilis ratione.
+	</p>
+</main>
+<?php include 'components/footer.php'; ?>
